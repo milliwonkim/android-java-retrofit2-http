a5ae4c505d7a8dc845bf2f8aae4f447ddfc22c9d
milliwonkim <rldnjs9347@gmail.com>
2020-11-09 21:45:49 +0900

    get

---
+++ app/src/main/java/com/example/myapplication/JsonPlaceholderApi.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceholderApi {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+}
